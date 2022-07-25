e0fb3f29cfa9131a28b0855f31b9e2583124160e
sathyanarayanan <sathyanarayanangunaseelan@gmail.com>
2022-07-25 14:15:16 +0530

    Another change

---
+++ add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=10
+b=15
+sum=(a+b)
+print (sum)
+
+
+# In[ ]:
+
+
+
+
---
+++ sub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=10
+b=15
+sub=(a-b)
+print (sub)
+
+
+# In[ ]:
+
+
+
+
